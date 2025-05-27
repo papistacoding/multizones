b930acb323e365fb68a49efb6fcdbd1a97d14fd4
papistacoding <bruno.papista@gmail.com>
2025-05-28 01:50:22 +0200

    adding env

--- apps/shell/next.config.ts
+++ apps/shell/next.config.ts
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
+const APP_A_URL = process.env.APP_A_URL;
+const APP_B_URL = process.env.APP_B_URL;
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/app-a",
-        destination: `http://localhost:3001/app-a`,
+        destination: `${APP_A_URL}/app-a`,
       },
       {
         source: "/app-a/:path+",
-        destination: `http://localhost:3001/app-a/:path+`,
+        destination: `${APP_A_URL}/app-a/:path+`,
       },
       {
         source: "/app-b",
-        destination: `http://localhost:3002/app-b`,
+        destination: `${APP_B_URL}/app-b`,
       },
       {
         source: "/app-b/:path+",
-        destination: `http://localhost:3002/app-b/:path+`,
+        destination: `${APP_B_URL}/app-b/:path+`,
       },
     ];
   },
